3718e778be15abc75da2d35dfdb80ddb0c8a4394
sakshi amnerkar <161360450+saksh678@users.noreply.github.com>
2024-10-09 00:09:59 +0530

    Add files via upload

---
+++ que2.js
@@ -0,0 +1,18 @@
+// Q2. Write a program that grades students based on their marks
+// If greater than 90 then A Grad
+// If between 70 and 90 then a B grad
+// If between 50 and 70 then a C grad
+// Below 50 then an F grade?
+// ans .
+x = prompt("enter your mark to show your grad:");
+// x show the grad of the student
+if(x > 90){
+    alert("congratulation you got `A` grad")
+}else if(x >= 70){x
+    alert("congratulation you got `B` grad")
+}else if(x >= 50){
+    alert("congratulation you got `C` grad `TRY HARDER`")
+}else {
+    alert("Sorry you got `F` grad `TRY HARDER better luck") 
+};
+// output :
